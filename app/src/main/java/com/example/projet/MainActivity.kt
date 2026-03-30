39b54011c185a9117cddbff8fee1f51977506192
FafaGr <grief.fares@gmail.com>
2026-03-31 00:14:08 +0200

    Test

--- app/src/main/java/com/example/projet/MainActivity.kt
+++ app/src/main/java/com/example/projet/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             ProjetTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "AE",
+                        name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
