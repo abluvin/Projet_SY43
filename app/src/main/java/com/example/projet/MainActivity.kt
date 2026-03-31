0729cdd5e6fb786cfc9791fd60b7adb2ede39172
FafaGr <grief.fares@gmail.com>
2026-03-31 16:40:14 +0200

    Premier Jet

--- app/src/main/java/com/example/projet/MainActivity.kt
+++ app/src/main/java/com/example/projet/MainActivity.kt
@@ -1,47 +1,134 @@
-package com.example.projet
+package com.example.class3
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Call
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.projet.ui.theme.ProjetTheme
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.class3.ui.theme.Class3Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            ProjetTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "AE",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            Class3Theme() {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = Color(0x000000)
+                ) {
+                    Full()
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ProjetTheme {
-        Greeting("Android")
+    @Composable
+    fun Full() {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            HeadSection()
+            Spacer(modifier = Modifier.height(400.dp))
+            BottomSection()
+        }
+    }
+
+
+    @Composable
+    fun HeadSection() {
+        val image = painterResource(R.drawable.utbm)
+
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = image,
+                contentDescription = null,
+                modifier = Modifier.size(100.dp)
+            )
+            Text(
+                text = "Bienvenue sur votre application",
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.Black
+            )
+            Text(
+                text = "MyUTBM",
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Medium,
+                color = Color.Blue
+            )
+        }
+    }
+
+    @Composable
+    fun BottomSection() {
+        Row(
+            modifier = Modifier.padding(0.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Section(icon = Icons.Default.Call, text = "Accueil")
+            Section(icon = Icons.Default.Info, text = "Emploi du temps")
+            Section(icon = Icons.Default.Email, text = "Compte")
+        }
+    }
+
+    @Composable
+    fun Section(icon: ImageVector, text: String) {
+        Column(
+            modifier = Modifier.padding(top = 150.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Icon(
+                imageVector = icon,
+                contentDescription = null,
+                tint = Color.Blue,
+                modifier = Modifier.size(24.dp)
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(text = text, fontSize = 16.sp, color = Color.Black)
+        }
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun FullPreview() {
+        Class3Theme {
+            Full()
+        }
     }
 }
+
